c2dc7165bb0769b166b2c0c33dde4f4841b4daa6
Sammy Algharazi <sainfo740@gmail.com>
2025-06-05 12:49:38 -0400

    added ui swagger to employee management system

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'attendence',
     'performance',
+    'drf_yasg',
 
 ]
 
--- employee_project/urls.py
+++ employee_project/urls.py
@@ -1,24 +1,34 @@
-"""
-URL configuration for employee_project project.
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.shortcuts import redirect
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+def redirect_to_docs(request):
+    return redirect('schema-swagger-ui')
 
-from django.urls import path, include
+# Swagger schema view config
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Employee Project API",
+      default_version='v1',
+      description="API documentation for Employee Management System",
+      contact=openapi.Contact(email="your@email.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path("", include("employees.urls")),
-    path("", include("attendence.urls")),
-    path("",include("performance.urls"))
+    path("admin/", admin.site.urls),
+    path("",redirect_to_docs),
+    path("employees/", include("employees.urls")),
+    path("attendence/", include("attendence.urls")),
+    path("performance/", include("performance.urls")),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
